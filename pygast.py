076dfc09c9fff551c1fd795df4eee7745a06f562
aib <aibok42@gmail.com>
2017-04-27 12:32:29 +0300

    Switch to HSL color mode

--- pygast.py
+++ pygast.py
@@ -32,6 +32,30 @@ uniform float u_time;
 
 varying vec2 v_texcoord;
 
+vec3 hsl2rgb(vec3 hsl) {
+	float Hp = hsl.x * 6;
+	float C = (1 - abs(2*hsl.z - 1)) * hsl.y;
+	float X = C * (1 - abs(mod(Hp, 2) - 1));
+	vec3 rgb1;
+
+	if (mod(Hp, 6) <= 1) {
+		rgb1 = vec3(C, X, 0);
+	} else if (mod(Hp, 6) <= 2) {
+		rgb1 = vec3(X, C, 0);
+	} else if (mod(Hp, 6) <= 3) {
+		rgb1 = vec3(0, C, X);
+	} else if (mod(Hp, 6) <= 4) {
+		rgb1 = vec3(0, X, C);
+	} else if (mod(Hp, 6) <= 5) {
+		rgb1 = vec3(X, 0, C);
+	} else {
+		rgb1 = vec3(C, 0, X);
+	}
+
+	float m = hsl.z - C/2;
+	return rgb1 + m;
+}
+
 void main()
 {
 	float x = v_texcoord.x;
@@ -41,8 +65,14 @@ void main()
 	float t1 = %(tree1)s;
 	float t2 = %(tree2)s;
 	float t3 = %(tree3)s;
-
-	gl_FragColor = vec4(t1, t2, t3, 1);
+/*
+	float xx = (8 * x) - 4;
+	float yy = (8 * y) - 4;
+	t1 = t / 100;
+	t2 = xx;
+	t3 = yy;
+*/
+	gl_FragColor = vec4(hsl2rgb(vec3(fract(t1), t2, t3 + 0.5)), 1);
 }
 """
 
